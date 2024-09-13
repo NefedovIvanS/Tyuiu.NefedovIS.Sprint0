e6d50722d25c0b2295aac8131cb1874a88781d9c
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-09-14 00:08:10 +0500

    Task6

---
+++ Tyuiu.NefedovIS.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿namespace Tyuiu.NefedovIS.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] number)
+        {
+            var total = 0;
+            for (var i = 0; i < number.Length; i++)
+            {
+                total = total + number[i];
+            }
+            return total;
+        }
+        public static object SubtructionArray(int[] number)
+        {
+            var total = 0;
+            int index = 0;
+
+            while (index < number.Length)
+            {
+                total = total - number[index];
+                index++;
+            }
+            return total;
+        }
+        public static object MultiplicationArray(int[] number)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * number[index];
+                index++;
+            }
+            while (index < number.Length);
+
+            return total;
+        }
+    }
+}
