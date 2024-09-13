e73aa860f5501eedd77dcf976ab50e6d36dec91b
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-09-13 20:28:01 +0500

    Task4

---
+++ Tyuiu.NefedovIS.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.NefedovIS.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtruction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
